a9f40474e7ab52bfea96b45de42cf24ffc9dba3d
Sean <32911193+yycyjqc@users.noreply.github.com>
2018-03-24 10:27:44 -0700

    delete something

--- twitter_config.py
+++
@@ -1,5 +0,0 @@
-# Twitter API Keys
-consumer_key = "7YDEuv1PH6aXatZoTx6e7tCps"
-consumer_secret = "5bDsKHPwCX3BBcFcQlsJRLshv1LsazBzjQTZzpgKdIb5AGZph7"
-access_token = "2577281960-pUr6mxKXy9uMIHVbvEm7u6x5wuvXJucFrdcH6iy"
-access_token_secret = "g55ck6EAa1svbN71srRuKeQlZtAmgRM1EsvenXG268iTJ"
